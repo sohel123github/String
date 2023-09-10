e368624413863256a9490904e05fa55a0743a67b
Sohel <patvekarsuhel@gmail.com>
2023-09-10 14:58:47 +0530

    First String commit

---
+++ String/src/com/string/Test.java
@@ -0,0 +1,98 @@
+package com.string;
+
+public class Test {
+//	static int a;
+//	String name;
+//	int b;
+//	float c;
+	
+	public static void main(String[] args) {
+//Code with Harry String Practical..		
+		/*String name = "Harry";
+//		String name = "Harryrry";
+		
+		System.out.println(name.length());
+		System.out.println(name.toLowerCase());
+		System.out.println(name.toUpperCase());
+		System.out.println(name.trim());
+		System.out.println(name.substring(3));
+		System.out.println(name.substring(1, 3));
+		System.out.println(name.replace('r', 'p'));
+		System.out.println(name.replace("rry", "ier"));
+		System.out.println(name.startsWith("Ha"));
+		System.out.println(name.endsWith("ry"));
+		System.out.println(name.charAt(2));
+		System.out.println(name.indexOf('y'));
+		System.out.println(name.indexOf("Ha"));
+		System.out.println(name.indexOf("rry", 4));
+		System.out.println(name.equals("Harry"));
+		System.out.println(name.equalsIgnoreCase("harry"));
+		*/
+	System.out.println("------------------------------------------------------");
+//		Smart Programing String Practical..
+	/*	String s1 = new String("deepak");
+		String s2 = new String("deepak");
+		
+//		s1=s2;
+		
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));*/
+		
+	/*	String s3 = "amit";
+		String s4 = "amol";
+		
+//		s3=s4;
+		
+		System.out.println(s3==s4);
+		System.out.println(s3.equals(s4));*/
+	/*
+	System.out.println("------------------------------------------------------");
+
+		String name = new String("Sohel");
+		System.out.println(name);
+		System.out.println(name.concat(" Patvekar"));
+		System.out.println(name);
+
+	System.out.println("------------------------------------------------------");
+
+		StringBuffer sb = new StringBuffer("Sohel");
+		System.out.println(sb);
+		System.out.println(sb.append(" Patvekar"));
+		System.out.println(sb);
+
+	System.out.println("------------------------------------------------------");
+		*/
+	
+//		Palindrome String Program..
+		/*
+		String name = "wow";
+		int l = name.length();
+		String rev = "";
+		for(int i = l - 1; i>=0; i--) {
+			rev = rev + name.charAt(i);
+		}
+		System.out.println(rev);
+		if(name.equals(rev)) {
+			System.out.println(name+" It is Palindrome String");
+		}else {
+			System.out.println(name+" It is Not Palindrome String");
+		}
+		*/
+//	System.out.println("------------------------------------------------------");
+
+/*		
+//		Here one objects created-
+//		one in scp as s1 & s2 point s1..
+	String s1 = "Sohel"; //one objects in scp 
+	String s2 = "Sohel"; //no objects s2 points s1
+		
+//		Here two objects created-
+//		one in heap & one in scp as st..
+	String st = new String("Raj"); //two objects heap & scp
+	String st1 = new String("Raj"); //one object in heap & in scp 
+									//st1 point st
+*/		
+		
+		
+	}
+}
